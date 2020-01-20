3ce6b738dba8f3c496fbf00d392b301714d0dfc7
sumesh <sumeshchandranofficial@gmail.com>
2020-01-20 20:38:17 +0530

    "get service established"

--- demo-chat/server.js
+++ demo-chat/server.js
@@ -1,7 +1,17 @@
 var express = require("express")
 var app = express();
 
+var messages = 
+[
+	{name:'Sumesh',message:'Hello human'},
+	{name:'Felix',message:'Hello 9 yr old'}
+]
+
 app.use(express.static(__dirname)) 
 var server = app.listen(3000, ()=>{
     console.log(" listening to the server from port ",server.address().port)
 });
+
+app.get('/main', (req,res)=>{
+	res.send(messages)
+}) 
