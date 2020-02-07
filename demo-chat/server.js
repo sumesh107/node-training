78d55f5ad33d59ce19f198b82b8e6520cc56fac0
sumesh <sumeshchandranofficial@gmail.com>
2020-02-07 21:21:05 +0530

    socket works

--- demo-chat/server.js
+++ demo-chat/server.js
@@ -1,15 +1,19 @@
 var express = require("express")
 var app = express();
 var parse = require("body-parser")
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
 app.use(parse.json())
+app.use(express.static(__dirname))
+app.use(parse.urlencoded({extended:false}))
 
 var messages = 
 [
 	{name:'Sumesh',message:'Hello human'},
 	{name:'Felix',message:'Hello 9 yr old'}
 ]
-
-app.use(express.static(__dirname)) 
+ 
 var server = app.listen(3000, ()=>{
     console.log(" listening to the server from port ",server.address().port)
 });
@@ -17,9 +21,10 @@ var server = app.listen(3000, ()=>{
 app.get('/main', (req,res)=>{
 	res.send(messages)
 }) 
-
+/*
 app.post('/main', (req,res)=>{
 	console.log(req.body);
 	messages.push(req.body);
 	res.sendStatus(200);
 })
+*/
