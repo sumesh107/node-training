34de2f18843c3729eebbbe55fe891adb3e0482ef
sumesh <sumeshchandranofficial@gmail.com>
2020-03-07 12:31:38 +0530

    stable socket

--- demo-chat/server.js
+++ demo-chat/server.js
@@ -2,7 +2,7 @@ var express = require("express")
 var app = express();
 var parse = require("body-parser")
 var http = require('http').Server(app)
-//var io = require('socket.io')(http)
+var io = require('socket.io')(http)
 
 app.use(parse.json())
 app.use(express.static(__dirname))
@@ -13,15 +13,16 @@ var messages =
 	{name:'Sumesh',message:'Hello human'},
 	{name:'Felix',message:'Hello 9 yr old'}
 ]
-
+//pushes message onto 'www.localhost:3000/main'
 app.get('/main', (req,res)=>{
 	res.send(messages)
 }) 
 
-var server = app.listen(3000, ()=>{
+var server = http.listen(3000, ()=>{
     console.log(" listening to the server from port ",server.address().port)
 });
 
+//pushing a request body to test Postman. 
 app.post('/main', (req,res)=>{
 	console.log(req.body);
 	messages.push(req.body);
