5f55dba70343dfdce66523a18e5cd9c33f9fb285
sumesh <sumeshchandranofficial@gmail.com>
2020-07-22 21:58:51 +0530

    adding to databse done

--- demo-chat/server.js
+++ demo-chat/server.js
@@ -3,10 +3,16 @@ var app = express();
 var parse = require("body-parser")
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(parse.json())
 app.use(express.static(__dirname))
 app.use(parse.urlencoded({extended:false}))
+
+var Message = mongoose.model('Message',{
+	name : String,
+	message : String
+})
 
 var messages = 
 [
@@ -14,24 +20,41 @@ var messages =
 	{name:'Felix',message:'Hello 9 yr old'}
 ]
 
-//pushes message onto 'www.localhost:3000/main'
-app.get('/main', (req,res)=>{
+var dbUrl = 'mongodb+srv://admin:admin@cluster0.ruz9g.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority';
+//database url 
+
+//pushes message onto 'www.localhost:3000/messages'
+app.get('/messages', (req,res)=>{
 	res.send(messages)
 }) 
 
-var server = http.listen(3000, ()=>{
-    console.log(" listening to the server from port ",server.address().port)
-});
+//connecting to mongoose
+mongoose.connect( dbUrl , { useUnifiedTopology: true , useNewUrlParser: true} , (err) => {
+	console.log(' mongoose is connected', err);
+})
 
 //pushing a request body to test Postman. 
-app.post('/main', (req,res)=>{
+app.post('/messages', (req,res)=>{
 	console.log(req.body);
-	messages.push(req.body);
-	io.emit('message',req.body);
-	res.sendStatus(200);
+
+	var mongo_msg = new Message( req.body );
+
+	mongo_msg.save( (err) => {
+		if (err)
+			sendStatus(500);
+			// 
+		messages.push(req.body);
+		io.emit('messaging',req.body);
+		res.sendStatus(200);
+	} )
+	
 })
 
 //socket connection 
 io.on('connection', (socket) => {
 	console.log("a user is connected by socket")
 })
+
+var server = http.listen(3000, ()=>{
+    console.log(" listening to the server from port ",server.address().port)
+});
